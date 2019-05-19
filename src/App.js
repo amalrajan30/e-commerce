21763df45816993d8f92e5d75d1515055e1f8e92
amalrajan30 <amalrajan30@gmail.com>
2019-05-19 17:57:39 +0530

    blah

--- src/App.js
+++ src/App.js
@@ -13,6 +13,23 @@ class App extends Component {
         <Main />
         <Footer />
       </div>
+      <div className="App">
+      <Row style={{ width: "40%", margin: 'auto', padding: '20px' }}>
+        <Col style={{ height: '20px' }} xs={6} lg={18}></Col>
+        <Col lg={6} xs={18} style={{ background: "#4834d4", height: '20px' }}></Col>
+      </Row>
+      <Row style={{ width: "40%", margin: 'auto' }}>
+        <Col>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam consequuntur illo debitis, soluta quos tempora libero minima nihil, voluptates exercitationem reprehenderit iusto! Aperiam sit reiciendis incidunt corrupti dolorem ea deserunt.
+        Animi aliquid ad, vero quia hic odio. Necessitatibus, facilis ratione hic tenetur incidunt ea vitae minus architecto fugit alias explicabo ut exercitationem placeat? Et, molestias veritatis! Sunt repudiandae aliquam fugit.
+        Esse mollitia fugiat nulla voluptatem explicabo harum amet delectus asperiores natus inventore, veniam praesentium nostrum ratione voluptate qui minima nesciunt, non minus, consectetur unde nam? Odio provident cupiditate velit voluptate.
+        Consequuntur aut ratione, et ipsam modi vero placeat asperiores atque rerum quibusdam maiores, facilis, similique aliquam? Explicabo odio aliquid culpa suscipit dicta error fugiat mollitia esse laboriosam! Debitis, ad! Sint.
+        Enim, reiciendis sit impedit tenetur dolorem corrupti ad asperiores minima quam laborum voluptatibus at suscipit saepe atque natus sapiente placeat officiis doloremque. Eaque sint molestiae nostrum amet optio iste rerum.</Col>
+      </Row>
+      <Row style={{ width: "40%", margin: 'auto', padding: '20px' }}>
+        <Col style={{ height: '20px' }} xs={6} lg={18}></Col>
+        <Col lg={6} xs={18} style={{ background: "#4834d4", height: '20px' }}></Col>
+      </Row>
+    </div>
     );
   }
 }
